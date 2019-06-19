7ac49f1d015c48023ea0287abb50954a09c585ac
Igor Puchkov <50378821+goblinanime@users.noreply.github.com>
2019-06-19 18:53:26 +0300

    Add files via upload

---
+++ dictionary/dictionary.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct dictionary
+{
+	string word;
+	string mean;
+	dictionary *next;
+};
+
+struct letters
+{
+	string word;
+	int factor;
+	letters *next;
+};
+
+void fin(dictionary *&head);
+void give_dictionary(dictionary *&head, string temp);
+void fin(letters *&head);
+void give_dictionary(letters *&head, string temp);
+void give_letters(letters *&head, string temp, int n);
+
+int main()
+{
+	dictionary *dicHead = NULL;
+	fin(dicHead);
+	letters *listHead = NULL;
+	fin(listHead);
+	letters *p = listHead;
+	while (p)
+	{
+		cout << p->word << " " << p->factor << endl;
+		p = p->next;
+	}
+	system("pause");
+	return 0;
+}
+
+void fin(dictionary *&head)
+{
+	dictionary *p;
+	int i = 0;
+	for (char x = 'A'; x <= 'Z'; x++)
+	{
+		stringstream ss;
+		ss << x << ".txt";
+		ifstream file(ss.str());
+		string line;
+		if (!file.is_open())
+		{
+			cout << "open error" << endl;
+		}
+		else
+		{
+			while (file)
+			{
+				getline(file, line);
+				give_dictionary(head, line);
+			}
+
+			file.close();
+
+		}
+
+	}
+}
+
+void give_dictionary(dictionary *&head, string temp)
+{
+	int i;
+	dictionary *p = new dictionary;
+	dictionary *q;
+	for (i = 0; i < temp.size() && temp[i] != ' ';i++)
+	{
+		p->word += temp[i];
+	}
+	for (i;i < temp.size();i++)
+	{
+		p->mean += temp[i];
+	}
+	p->next = NULL;
+	if (!head)
+	{
+		head = p;
+		return;
+	}
+	q = head;
+	while (q->next)
+	{
+		q = q->next;
+	}
+	q->next = p;
+}
+
+void fin(letters *&head)
+{
+	string temp;
+	int n, num;
+	ifstream f1("letters.txt", ios::in);
+	ifstream f2("factors.txt", ios::in);
+	ifstream f3("count.txt", ios::in);
+	if (!f1.is_open() || !f2.is_open() || !f3.is_open())
+	{
+		cout << "open error" << endl;
+	}
+	else
+	{
+		while (f1 && f2 && f3)
+		{
+			f3 >> n;
+			getline(f1, temp);
+			f2 >> num;
+			for (int i = 0;i < n;i++)
+			{
+				give_letters(head, temp, num);
+			}
+		}
+
+		f1.close();
+		f2.close();
+		f3.close();
+
+	}
+}
+void give_letters(letters *&head, string temp, int n)
+{
+	letters *p = new letters;
+	letters *q;
+	p->word = temp;
+	p->factor = n;
+	p->next = NULL;
+	if (!head)
+	{
+		head = p;
+		return;
+	}
+	q = head;
+	while (q->next)
+	{
+		q = q->next;
+	}
+	q->next = p;
+}
