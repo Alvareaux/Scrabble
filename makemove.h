5465c75f7ddffdd283e813df656c50ab7ed49c01
Igor Puchkov <50378821+goblinanime@users.noreply.github.com>
2019-06-20 17:09:13 +0300

    Update makemove.h

--- makemove.h
+++ makemove.h
@@ -6,7 +6,7 @@ using namespace std;
 
 void give_list(letters *&, string, int);
 void make_move(letters *&, letters *&, dictionary *);
-void input_letters(letters *, letters *&);
+void input_word(letters *, string&);
 void rand_letter(letters *&, letters *&);
 
 #endif
