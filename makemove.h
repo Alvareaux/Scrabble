1c93a18c5c2ada19433e605f0857f5343e7c88ad
Igor Puchkov <50378821+goblinanime@users.noreply.github.com>
2019-06-20 13:02:48 +0300

    Update makemove.h

--- makemove.h
+++ makemove.h
@@ -1,8 +1,12 @@
 #include <string>
 #include "dictionary.h"
 using namespace std;
+#ifndef MAKEMOVE_H
+#define MAKEMOVE_H
 
-void give_list(letters *&listHead, string word, int factor);
-void make_move(letters *&listHead, letters *&letterHead, dictionary *dicHead);
-void input_letters(letters *listHead, letters *&wordHead);
-void rand_letter(letters *&listHead, letters *&letterHead);
+void give_list(letters *&, string, int);
+void make_move(letters *&, letters *&, dictionary *);
+void input_letters(letters *, letters *&);
+void rand_letter(letters *&, letters *&);
+
+#endif
