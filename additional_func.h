6dc36e4c835ce8757ee2cf64824f7db166ba7ed6
Igor Puchkov <50378821+goblinanime@users.noreply.github.com>
2019-06-20 13:02:20 +0300

    Update additional_func.h

--- additional_func.h
+++ additional_func.h
@@ -1,8 +1,13 @@
 #include <string>
 #include "dictionary.h"
 using namespace std;
-
+#ifndef ADDITIONAL_FUNC_H
+#define ADDITIONAL_FUNC_H
 void delete_letter(letters *&head, letters *x);
 void letters_output(letters *head);
 int list_counter(letters *head);
 int check(dictionary *head, letters *wordHead);
+int check(dictionary *head, string word);
+#endif
+
+
