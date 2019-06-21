e4406e1a7aa0f94285734990463a7f4280ddeef8
Igor Puchkov <50378821+goblinanime@users.noreply.github.com>
2019-06-21 22:48:13 +0300

    Update game_func.h

--- lib/game_func.h
+++ lib/game_func.h
@@ -5,14 +5,15 @@ using namespace std;
 
 #include "../dictionary/dictionary.h"
 #include "additional_func.h"
+#include "misc_func.h"
 
 struct player
 {
-    int id;
+	int id;
 
-    letters *list;
+	letters *list;
 
-    int score;
+	int score;
 };
 
 void DisplayPlayground(string **playground, int **area, int n = 15);
@@ -20,10 +21,13 @@ void RandLetter(letters *&listHead, letters *&letterHead);
 void GiveHand(player &p, letters *&letterHead);
 void OutputHand(player *p);
 void InputWord(letters *listHead, string &word);
-void MakeMove(string **playground, int **area, letters *&listHead, dictionary *dictHead, player *&p);
+void MakeMove(string **playground, int **area, letters *&listHead, dictionary *dictHead, player *&p, player *q);
 int CheckDirection(int len, int x, int y, string direction);
-int PullWord(string **playground, int **area, int x, int y, string direction, string word);
-void ChangePlayer(player *&active, player *first, player *second);
+int PullWord(dictionary *head, string **playground, int **area, int x, int y, string direction, string word);
+void ChangePlayer(player *&active, player *&passive, player *first, player *second);
 void SwapHand(player *&active, letters *&letterList, string swap);
+int CheckVoid(string **arr, int y, int x);
+int VerticalCheck(dictionary *head, string **arr, string direction, int y, int x);
+int HorizontalCheck(dictionary *head, string **arr, string direction, int y, int x);
 
 #endif //SCRABBLE_GAME_FUNC_H
