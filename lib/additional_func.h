b4a271e2fd5586c19d138756ee13192d85942e7b
Igor Puchkov <50378821+goblinanime@users.noreply.github.com>
2019-06-22 00:11:39 +0300

    Update additional_func.h

--- lib/additional_func.h
+++ lib/additional_func.h
@@ -10,5 +10,6 @@ void DeleteLetter(letters *&head, string x);
 
 int Check(letters *head, string word);
 int Check(dictionary *head, string word);
+void Clone(letters *head1, letters *&head2);
 
 #endif //SCRABBLE_ADDITIONAL_FUNC_H
