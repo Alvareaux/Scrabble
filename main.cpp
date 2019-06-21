4057253add8c383e5863e04388ec1d2e1da47381
Igor Puchkov <50378821+goblinanime@users.noreply.github.com>
2019-06-21 22:46:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,24 +12,23 @@
 #include "lib/additional_func.h"
 
 using namespace std;
-
 const int N = 15;
 
 int main()
 {
-    srand(time(0));
+	srand(time(0));
 
-	string path = "C:\\CPrj\\Scrabble\\area.txt";
+	string path = "area.txt";
 
 	string **playground = CreateMatrixString(N);
 
 	for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            *(*(playground + i) + j) = " ";
-        }
-    }
+	{
+		for (int j = 0; j < N; j++)
+		{
+			*(*(playground + i) + j) = " ";
+		}
+	}
 
 	int **area = CreateMatrixInt(N);
 
@@ -38,99 +37,93 @@ int main()
 		cout << "SCRABBLE" << endl;
 		cout << "Game by Anatolii Voitko and Ihor Nekhaienko" << endl;
 		cout << "https://github.com/Alvareaux/Scrabble" << endl;
-        cout << "\t[TP-82]" << endl;
+		cout << "\t[TP-82]" << endl;
 
-        cout << "\t\nLoading\n" << endl;
+		cout << "\t\nLoading...\n" << endl;
 
-        dictionary *dictHead = NULL;
-        fin(dictHead);
-        letters *letterHead = NULL;
-        fin(letterHead);
+		dictionary *dictHead = NULL;
+		fin(dictHead);
+		letters *letterHead = NULL;
+		fin(letterHead);
 
-        player first;
-        player second;
+		player first;
+		player second;
 
-        first.id = 1;
-        second.id = 2;
-        first.score = 0;
-        second.score = 0;
+		first.id = 1;
+		second.id = 2;
+		first.score = 0;
+		second.score = 0;
 
-        first.list = NULL;
-        second.list = NULL;
-        GiveHand(first, letterHead);
-        GiveHand(second, letterHead);
+		first.list = NULL;
+		second.list = NULL;
+		GiveHand(first, letterHead);
+		GiveHand(second, letterHead);
 
-        player *active = &first;
+		player *active = &first;
+		player *passive = &second;
+		cout << "Game features: " << endl;
+		cout << "\tBlue numbers are word multipliers" << endl;
+		cout << "\tWhite numbers are letter multipliers\n" << endl;
 
-        cout << "Game features: " << endl;
-        cout << "\tBlue numbers are word multipliers" << endl;
-        cout << "\tWhite numbers are letter multipliers\n" << endl;
+		system("pause");
 
-        system("pause");
+		int turn = 1;
+		do
+		{
+			system("CLS");
 
-        int turn = 1;
-        do
-        {
-            system("CLS");
+			cout << "SCRABBLE";
+			cout << "\n\tTurn " << turn << "\n";
+			cout << "\tPlayer " << active->id << "\n\n";
 
-            cout << "SCRABBLE" << endl;
-            cout << "Game by Anatolii Voitko and Ihor Nekhaienko" << endl;
-            cout << "\n\tTurn " << turn << "\n";
-            cout << "\tPlayer " << active -> id << "\n\n";
+			cout << "\tPlayer 1 score: " << first.score << endl;
+			cout << "\tPlayer 2 score: " << second.score << endl;
 
-            cout << "\tPlayer 1 score: " << first.score << endl;
-            cout << "\tPlayer 2 score: " << second.score << endl;
+			cout << endl;
 
-            cout << endl;
+			DisplayPlayground(playground, area);
 
-            DisplayPlayground(playground, area);
+			cout << "\n\tYour hand:" << endl;
+			OutputHand(active);
 
-            cout << "\n\tYour hand:" << endl;
-            OutputHand(active);
+			MakeMove(playground, area, letterHead, dictHead, active, passive);
+			GiveHand(*active, letterHead);
 
-            MakeMove(playground, area, letterHead, dictHead, active);
-            GiveHand(*active, letterHead);
+			system("pause");
 
-            system("pause");
+			ChangePlayer(active,passive, &first, &second);
+			turn++;
+		} while (letterHead);
 
-            ChangePlayer(active, &first, &second);
-            turn++;
-        }
-        while (letterHead);
+		system("CLS");
 
-        system("CLS");
+		cout << "SCRABBLE";
+		cout << "\n\tTurn " << turn << "\n";
+		cout << "\tPlayer " << active->id << "\n\n";
 
-        cout << "SCRABBLE";
-        cout << "\n\tTurn " << turn << "\n";
-        cout << "\tPlayer " << active -> id << "\n\n";
+		cout << "\tPlayer 1 score: " << first.score << endl;
+		cout << "\tPlayer 2 score: " << second.score << endl;
 
-        cout << "\tPlayer 1 score: " << first.score << endl;
-        cout << "\tPlayer 2 score: " << second.score << endl;
+		cout << endl;
 
-        cout << endl;
+		DisplayPlayground(playground, area);
 
-        DisplayPlayground(playground, area);
+		cout << endl;
 
-        cout << endl;
-
-        if (first.score > second.score)
-        {
-            cout << "\tPlayer 1 win!";
-        }
-        else if (first.score < second.score)
-        {
-            cout << "\tPlayer 2 win!";
-        }
-        else if (first.score == second.score)
-        {
-            cout << "\tDead heat!";
-        }
+		if (first.score > second.score)
+		{
+			cout << "\tPlayer 1 win!";
+		}
+		else if (first.score < second.score)
+		{
+			cout << "\tPlayer 2 win!";
+		}
+		else if (first.score == second.score)
+		{
+			cout << "\tDead heat!";
+		}
 	}
 
 	system("pause");
 	return 0;
 }
-
-
-
-
